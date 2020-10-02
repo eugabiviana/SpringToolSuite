1668efeb6a8d9b80a0ec027cd41de96ef4290de2
unknown <gabi.viana88@gmail.com>
2020-10-02 14:46:51 -0300

    Aula 01-10

--- servico-api-1-dia/src/main/java/com/servico/demo/Controller.java
+++ servico-api-1-dia/src/main/java/com/servico/demo/Controller.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,4 +28,15 @@ public class Controller {
 		return model;
 	}
 	
+	@GetMapping("/servicos/nome/{nome}")
+	public List<ServicoModel> buscarPorNome(@PathVariable String nome){
+		return repository.findByNome(nome);
+	}
+	
+	@PutMapping("/servicos/{id}")
+	public ServicoModel atualizar(@PathVariable Long id, @RequestBody ServicoModel model) {
+		model.setId(id);
+		repository.save(model);
+		return model;
+	}
 }
--- servico-api-1-dia/src/main/java/com/servico/demo/ServicoRepository.java
+++ servico-api-1-dia/src/main/java/com/servico/demo/ServicoRepository.java
@@ -1,8 +1,11 @@
 package com.servico.demo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ServicoRepository extends JpaRepository<ServicoModel, Long>{
 
+	public List<ServicoModel> findByNome(String string);
 	
 }
