8f106b8361a025b9db0463cef1071cda3c4249ad
eugabiviana <qqcoisa@g>
2020-10-07 19:12:39 -0300

    Creating the new model Tema

--- servico-api-1-dia/src/main/resources/controller.js
+++ servico-api-1-dia/src/main/resources/controller.js
@@ -1,8 +1,7 @@
 var app = angular.module('myApp', []);
 
 app.controller('myCtrl', function($scope, $http) {
-	$scope.id = "1";
-	$scope.nome = "Generation";
+	$scope.servicos = new Object();
 
 	$scope.salvar = function() {
 		$http.post("http://localhost:8080/servicos", {
@@ -12,10 +11,11 @@ app.controller('myCtrl', function($scope, $http) {
 		})
 	};
 
-	$scope.get = function() {
+	$scope.getAll = function() {
 		$http.get("http://localhost:8080/servicos").then(function(resposta) {
-			console.info(resposta);
+			$scope.servicos = resposta.data;
 		});
 	}
 
+	$scope.getAll();
 });
