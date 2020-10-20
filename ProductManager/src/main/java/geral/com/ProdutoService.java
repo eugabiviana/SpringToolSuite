a3577b8c3e8732a70602c40e226ce611353a5e8c
eugabiviana <qqcoisa@g>
2020-10-20 17:29:46 -0300

    Deploy and Jar

---
+++ ProductManager/src/main/java/geral/com/ProdutoService.java
@@ -0,0 +1,25 @@
+package geral.com;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class ProdutoService {
+
+	@Autowired
+	private ProdutoRepository repo;
+	
+
+	public void save(TableProduto produto) {
+		repo.save(produto);
+	}
+	
+	public List<TableProduto> listALL(){
+		return repo.findAll();
+	}
+	
+}
